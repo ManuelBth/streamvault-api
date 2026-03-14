8377e11100dc54501d35de9627b839221ef4bade
mbth <manuelbth@outlook.com>
2026-03-14 11:58:37 -0500

    feat: add project structure and pom.xml with dependencies

---
+++ project/streamvault-api/src/main/java/com/betha/streamvault/StreamVaultApplication.java
@@ -0,0 +1,12 @@
+package com.betha.streamvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamVaultApplication.class, args);
+    }
+}
